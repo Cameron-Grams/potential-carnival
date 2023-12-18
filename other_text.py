6f3e3f77f0ce247695109e8409af11bd98407989
Cameron <cameron.grams@gmail.com>
2023-12-17 20:07:34 -0500

    additional file

---
+++ other_text.py
@@ -0,0 +1,75 @@
+non_wiki = [('Maya_Angelou',
+        """You may write me down in history
+With your bitter, twisted lies,
+You may tread me in the very dirt
+But still, like dust, I'll rise.
+
+Does my sassiness upset you?
+Why are you beset with gloom?
+'Cause I walk like I've got oil wells
+Pumping in my living room.
+
+Just like moons and like suns,
+With the certainty of tides,
+Just like hopes springing high,
+Still I'll rise.
+
+Did you want to see me broken?
+Bowed head and lowered eyes?
+Shoulders falling down like teardrops.
+Weakened by my soulful cries.
+
+Does my haughtiness offend you?
+Don't you take it awful hard
+'Cause I laugh like I've got gold mines
+Diggin' in my own back yard.
+
+You may shoot me with your words,
+You may cut me with your eyes,
+You may kill me with your hatefulness,
+But still, like air, I'll rise.
+
+Does my sexiness upset you?
+Does it come as a surprise
+That I dance like I've got diamonds
+At the meeting of my thighs?
+
+Out of the huts of history's shame
+I rise
+Up from a past that's rooted in pain
+I rise
+I'm a black ocean, leaping and wide,
+Welling and swelling I bear in the tide.
+Leaving behind nights of terror and fear
+I rise
+Into a daybreak that's wondrously clear
+I rise
+Bringing the gifts that my ancestors gave,
+I am the dream and the hope of the slave.
+I rise
+I rise
+I rise."""), ('Robert_Frost',
+    """Two roads diverged in a yellow wood,
+And sorry I could not travel both
+And be one traveler, long I stood
+And looked down one as far as I could
+To where it bent in the undergrowth;
+
+Then took the other, as just as fair,
+And having perhaps the better claim
+Because it was grassy and wanted wear,
+Though as for that the passing there
+Had worn them really about the same,
+
+And both that morning equally lay
+In leaves no step had trodden black.
+Oh, I kept the first for another day!
+Yet knowing how way leads on to way
+I doubted if I should ever come back.
+
+I shall be telling this with a sigh
+Somewhere ages and ages hence:
+Two roads diverged in a wood, and I,
+I took the one less traveled by,
+And that has made all the difference.""")]
+
